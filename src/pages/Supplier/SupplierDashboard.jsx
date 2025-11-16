c90ef65140673114d9df740d954a17e4ac873176
Sanzhar Mussainov <sanzhar.mussainov@nu.edu.kz>
2025-11-16 21:38:01 +0500

    ez

--- src/pages/Supplier/SupplierDashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function SupplierDashboard() {
-  return (
-    <div style={{ paddingTop: "6rem", textAlign: "center" }}>
-      <h2>Supplier Dashboard</h2>
-      <p>Manage your catalog, orders, and complaints here.</p>
-    </div>
-  );
-}
