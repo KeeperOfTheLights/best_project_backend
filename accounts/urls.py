d1e28df19c883e437e90a21afcb3ccbda9d7da57
Sanzhar Mussainov <sanzhar.mussainov@nu.edu.kz>
2025-11-16 22:21:16 +0500

    product endpoint added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, SupplierProductListCreateView, SupplierProductUpdateDeleteView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('products/', SupplierProductListCreateView.as_view(), name='supplier-products'),
+    path('products/<int:pk>/', SupplierProductUpdateDeleteView.as_view(), name='supplier-product-detail'),
 ]
