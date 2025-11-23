bf973e916a046de032bc5bb14167e0099c2b8b29
Sanzhar Mussainov <sanzhar.mussainov@nu.edu.kz>
2025-11-23 12:13:25 +0500

    fixed minor errors

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,3 @@
-from django.db import models
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
--- best_project/urls.py
+++ best_project/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import re_path as url
 from django.conf import settings
 from django.conf.urls.static import static
-from main.views import YoutubeVideoAPIView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
