2b446fab548857dd6b8d9a556a259701f150cb23
Sanzhar Mussainov <sanzhar.mussainov@nu.edu.kz>
2025-11-26 16:08:27 +0500

    fixing

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -4,6 +4,7 @@ class IsOwner(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == "owner"
 
+
 class IsManager(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == "manager"
